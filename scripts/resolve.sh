e1f28bb1efa840d2e53873023e11486ecab31a7e
James Young <hello@jamesyoung.ch>
2020-11-10 16:07:46 +0100

    chore: update resolve script to accept output path
    When resolving, the output path is used is specified, else a temp
    file is created. The new `resolve:for-mvn` npm script is added for
    convenience when building; the output location is where the maven
    generation script expects the input file.

--- scripts/resolve.sh
+++ scripts/resolve.sh
@@ -1,15 +1,23 @@
 #! /bin/sh
 
 # This script runs the speccy resolver and outputs a single merged OAS3 spec file.
+# If a path is provided as the first argument, the resolved spec is saved there. Otherwise, a temp file is created.
 # This script can be ignored when using the lint and serve scripts, as this process is handled by them internally.
 
-# create temp file (so we can delete it later)
-tmpfile=$(mktemp)
+if [ -z "$1" ] ; then
+    echo "No path argument supplied; resolving spec to a temporary file."
+    file=$(mktemp) # create temp file
+else
+  # path specified. now we check it exists
+  path=$(echo "$1" | sed -E "s/(.+)\/.+/\1/")
+  mkdir -p $path
+  file=$1
+fi
 
 # merge all the spec files into out (resolves the $ref links)
-yarn speccy resolve ./src/spec/index.yaml -o "$tmpfile" --quiet
+yarn speccy resolve ./src/spec/index.yaml -o "$file" --quiet
 
 # serve the output
-echo "OAS3 Spec resolved to:  $tmpfile"
+echo "OAS3 Spec resolved to:  $file"
 
 # we leave the temp file cleanup to the OS
